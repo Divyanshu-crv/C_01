902a5ad45f7b93cb74f4b6bd9a47676587c3f334
Divyanshu-crv <crvidyapeeth@gmail.com>
2026-06-30 14:47:02 +0530

    only basic structure

---
+++ js/data.js
@@ -0,0 +1,311 @@
+const quickAccess = [
+
+    {
+        name: "C LANG Telegram",
+        url: "#"
+    }
+
+];
+
+const sections = [
+
+      {
+        title: "Important",
+        icon: "🔒",
+
+        categories: [
+
+            {
+                title: "Some Important",
+
+                subcategories: [
+
+                    {
+                        title: "Updation",
+
+                        items: [
+
+                            {
+                                name: "Website Update",
+                                url: "./assets/Collection_03update.pdf",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Random(Perplexity)",
+                                url: "https://www.perplexity.ai/",
+                                type: "link"
+                            }
+
+                        ]
+
+                    }
+
+                ]
+
+            }
+
+        ]
+    },
+
+        {
+        title: "Sample_1",
+        icon: "🛍️",
+
+        categories: [
+
+            {
+                title: "❌ MATH",
+
+                subcategories: [
+
+                    {
+                        title: "Weight and Capacity",
+
+                        items: [
+
+                            {
+                                name: "Video",
+                                url: "https://t.me/c/3966263884/10",
+                                type: "link"
+                            },
+
+                            {
+                                name: "📄 Class Notes",
+                                url: "https://t.me/c/3966263884/11",
+                                type: "link"
+                            },
+
+
+                            {
+                                name: "⭐  Important Questions",
+                                url: "https://t.me/c/3966263884/12",
+                                type: "link"
+                            },
+
+
+                            {
+                                name: "📚  DPP",
+                                url: "https://t.me/c/3966263884/13",
+                                type: "link"
+                            }
+
+
+                        ]
+
+                    },
+
+                    {
+                        title: "Coconut Farm",
+
+                        items: [
+
+
+                            {
+                                name: "Video",
+                                url: "https://t.me/c/3966263884/15",
+                                type: "link"
+                            },
+
+                            {
+                                name: "📄 Class Notes",
+                                url: "https://t.me/c/3966263884/16",
+                                type: "link"
+                            },
+
+
+                            {
+                                name: "📝  Handwritten Notes",
+                                url: "https://t.me/c/3966263884/19",
+                                type: "link"
+                            },
+
+                            {
+                                name: "⭐  Important Questions",
+                                url: "https://t.me/c/3966263884/20",
+                                type: "link"
+                            },
+
+
+                            {
+                                name: "📚  DPP",
+                                url: "https://t.me/c/3966263884/21",
+                                type: "link"
+                            }
+
+
+                        ]
+
+                    },
+
+
+                    {
+                        title: "Symmetrical Designs",
+
+                        items: [
+
+
+                            {
+                                name: "Video",
+                                url: "https://t.me/c/3966263884/23",
+                                type: "link"
+                            },
+
+                            {
+                                name: "🛍️ Notes",
+                                url: "https://t.me/c/3966263884/32?single",
+                                type: "link"
+                            }
+
+
+                        ]
+
+                    },
+
+                    {
+                        title: "Grandmother's Quilt",
+
+                        items: [
+
+
+                            {
+                                name: "Video",
+                                url: "https://t.me/c/3966263884/33",
+                                type: "link"
+                            },
+
+                            {
+                                name: "🛍️ Notes",
+                                url: "https://t.me/c/3966263884/34?single",
+                                type: "link"
+                            }
+
+
+                        ]
+
+                    },
+
+                    {
+                        title: "Racing Seconds",
+
+                        items: [
+
+
+                            {
+                                name: "Video",
+                                url: "https://t.me/c/3966263884/38",
+                                type: "link"
+                            },
+
+                            {
+                                name: "🛍️ Notes",
+                                url: "https://t.me/c/3966263884/39?single",
+                                type: "link"
+                            }
+
+
+                        ]
+
+                    },
+
+
+                    {
+                        title: "Animal Jumps",
+
+                        items: [
+
+
+                            {
+                                name: "Video",
+                                url: "https://t.me/c/3966263884/43",
+                                type: "link"
+                            },
+
+                            {
+                                name: "🛍️ Notes",
+                                url: "https://t.me/c/3966263884/44?single",
+                                type: "link"
+                            }
+
+
+                        ]
+
+                    },
+
+                    {
+                        title: "Maps and Locations + Data Through Pictures",
+
+                        items: [
+
+
+                            {
+                                name: "Video",
+                                url: "https://t.me/c/3966263884/48",
+                                type: "link"
+                            },
+
+                            {
+                                name: "🛍️ Notes",
+                                url: "https://t.me/c/3966263884/49?single",
+                                type: "link"
+                            }
+
+
+                        ]
+
+                    },
+
+
+
+                ]
+
+            }
+        ]
+    },
+
+
+    {
+        title: "Sample_2",
+        icon: "🔖",
+
+        categories: [
+
+            {
+                title: "AI",
+
+                subcategories: [
+
+                    {
+                        title: "Tools",
+
+                        items: [
+
+                            {
+                                name: "ChatGPT",
+                                url: "https://chatgpt.com",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Perplexity",
+                                url: "https://www.perplexity.ai/",
+                                type: "link"
+                            }
+
+                        ]
+
+                    }
+
+                ]
+
+            }
+
+        ]
+    },
+
+//    ==================================================================== 
+
+    // ---------------------------------------------------------------
+  
+
+];
